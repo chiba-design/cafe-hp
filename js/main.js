937f0c8a862f54021dda81a5c19f3bdd953e1795
chiba-design <112089127+chiba-design@users.noreply.github.com>
2025-01-11 11:51:16 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function() {
+    
+    // スムーススクロール
+    
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr("href");
+    
+        let target = $(href == "#" || href == "" ? "html" : href);
+    
+        let position = target.offset().top;
+    
+        $("html, body").animate({scrollTop: position}, 600, "swing");
+    });
+    
+    $(window).scroll(function () {
+    
+        $(".fadein").each(function () {   
+    
+        let scroll = $(window).scrollTop();
+    
+        let target = $(this).offset().top;
+    
+    
+        let windowHeight = $(window).height();
+    
+        if (scroll > target - windowHeight + 200) {
+    
+            
+            $(this).css("opacity", "1");
+            $(this).css("transform", "translateY(0)");
+    }
+        });
+    });
+    });
